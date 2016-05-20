c67449355a9dcf2982fd93249f91c10f010d0410
Billard <foresome1249@gmail.com>
2016-05-19 15:37:34 -1000

    Update and rename tf2schema.php to schema_test.php

---
+++ schema_test.php
@@ -0,0 +1,5 @@
+$appid = "440";
+$apikey = "7CDF4843997AB718BB3735429E127131";
+$schema = "http://api.steampowered.com/IEconItems_$appid/GetSchema/v0001/?key=$apikey";
+$json = jsondecode(filegetcontents($schema), true);
+echo $json["result"]["qualities"][0];
--- tf2schema.php
+++
@@ -1,4 +0,0 @@
-$appid = "440"
-$apikey = "7CDF4843997AB718BB3735429E127131"
-
-$schema = "http://api.steampowered.com/IEconItems_$appid/GetSchema/v0001/?key=$apikey"
